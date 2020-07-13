412d04e797a9bc8875a8ab2067ce8970160a1981
tenglong <*Wjp3158*>
2020-07-13 11:16:44 +0800

    非称重商品规格里面的重量限制为必填

--- app/src/main/java/com/same/part/assistant/viewmodel/state/EditProductSpecViewModel.kt
+++ app/src/main/java/com/same/part/assistant/viewmodel/state/EditProductSpecViewModel.kt
@@ -195,14 +195,14 @@ class EditProductSpecViewModel(application: Application) : BaseViewModel(applica
                 return result
             }
 
-//            if (productType.value != 1) {
-//                if (editProductSku.weight.isEmpty()) {
-//                    if (showToast) {
-//                        ToastUtils.showShort("规格${editProductSku.propertyString}的重量不能为空")
-//                    }
-//                    result = false
-//                    return result
-//                }
+            if (productType.value != 1) {
+                if (editProductSku.weight.isEmpty()) {
+                    if (showToast) {
+                        ToastUtils.showShort("规格${editProductSku.propertyString}的重量不能为空")
+                    }
+                    result = false
+                    return result
+                }
 //                if (editProductSku.barcode.isEmpty()) {
 //                    if (showToast) {
 //                        ToastUtils.showShort("规格${editProductSku.propertyString}的商品条码不能为空")
@@ -210,7 +210,7 @@ class EditProductSpecViewModel(application: Application) : BaseViewModel(applica
 //                    result = false
 //                    return result
 //                }
-//            }
+            }
         }
 
         if (result && productType.value != 1) {
